28148ed2e9fb1570aa9ebc82d67314e2e83ee133
sacharn <sacharn@163.com>
2024-05-12 23:03:39 +0800

    提交信息

---
+++ app/about/page.js
@@ -0,0 +1,118 @@
+import React from 'react'
+import Navbar from "@/components/Navbar";
+
+const page = () => {
+    return (
+        <div>
+            <Navbar />
+            <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+                <li>
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-end mb-10">
+                        <time className="font-mono italic">2022</time>
+                        <div className="text-lg font-black">工作</div>
+                        就职于华为终端BG-主要负责服务产品
+                    </div>
+                    <hr />
+                </li>
+
+                <li>
+
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-start md:text-end mb-10">
+                        <time className="font-mono italic">2019</time>
+                        <div className="text-lg font-black">研究生</div>
+                        就读于南京邮电大学-通信与信息工程学院-电子与通信工程专业
+                    </div>
+                    <hr />
+                </li>
+
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-end mb-10">
+                        <time className="font-mono italic">2014</time>
+                        <div className="text-lg font-black">大学</div>
+                        就读于南京工程学院-通信工程学院-电子与信息工程专业
+                    </div>
+                    <hr />
+                </li>
+
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-start md:text-end mb-10">
+                        <time className="font-mono italic">2011</time>
+                        <div className="text-lg font-black">高中</div>
+                        就读于江苏省口岸中学
+                    </div>
+                    <hr />
+                </li>
+
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-end mb-10">
+                        <time className="font-mono italic">2008</time>
+                        <div className="text-lg font-black">初中</div>
+                        就读于泰州实验学习
+                    </div>
+                    <hr />
+                </li>
+
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-start md:text-end mb-10">
+                        <time className="font-mono italic">2002</time>
+                        <div className="text-lg font-black">小学</div>
+                        先后就读于蔡堡中心小学和陈堡中心小学
+                    </div>
+                    <hr />
+                </li>
+
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-end mb-10">
+                        <time className="font-mono italic">2000</time>
+                        <div className="text-lg font-black">幼儿园</div>
+                        就读于蔡堡中心幼儿园
+                    </div>
+                    <hr />
+                </li>
+
+
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    </div>
+                    <div className="timeline-start md:text-end mb-10">
+                        <time className="font-mono italic">1996</time>
+                        <div className="text-lg font-black">出生</div>
+                        出生于江苏省兴化市
+                    </div>
+
+                </li>
+
+            </ul>
+        </div>
+    )
+}
+
+export default page
